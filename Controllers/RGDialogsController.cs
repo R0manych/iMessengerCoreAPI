8edc5100ab358c6c29d78bc8ef03c3cb9104e00b
Roman Chernigovskikh <roman_chernigovskikh@relex.ru>
2021-08-20 11:42:04 +0300

    Initial commit

---
+++ Controllers/RGDialogsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iMessengerCoreAPI.Models;
+using iMessengerCoreAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iMessengerCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RGDialogsController : ControllerBase
+    {
+        private RGDialogService _dialogService { get; set; }
+
+        public RGDialogsController()
+        {
+            _dialogService = new RGDialogService();
+        }
+
+        /// <summary>
+        ///  Поиск диалога, в котором участвуют все клиенты из списка.
+        /// </summary>
+        /// <param name="clientIds">Cписок идентфикаторов клиентов для которых необходимо найти диалог</param>
+        /// <returns>Список диалогов в котором есть все указанные клиенты.</returns>
+        [HttpPost]
+        public IEnumerable<Guid> GetRGDialogs ([FromBody]IEnumerable<Guid> clientIds)
+        {
+            return _dialogService.SearchRGDialogsForClients(clientIds);                        
+        }          
+    }
+}
---
+++ Services/RGDialogService.cs
@@ -0,0 +1,45 @@
+﻿using iMessengerCoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iMessengerCoreAPI.Services
+{
+    /// <summary>
+    /// Сервис для работы с диалогами пользователей.
+    /// </summary>
+    public class RGDialogService
+    {
+        private List<RGDialogsClients> _clients { get; set; }
+
+        public RGDialogService()
+        {
+            var client = new RGDialogsClients();
+            _clients = client.Init();
+        }
+
+        /// <summary>
+        /// Поиск диалога, в котором участвуют все клиенты из списка.
+        /// </summary>
+        /// <param name="clientIds">Список клиентов диалога.</param>
+        /// <returns>Список диалогов в котором есть все клиенты из списка</returns>
+        public IEnumerable<Guid> SearchRGDialogsForClients(IEnumerable<Guid> clientIds)
+        {
+            var dialogsWithClientsIds = _clients.Select(p => p.IDRGDialog).Distinct();
+            var dialogsWithClients = new List<RGDialogsClients>();
+            int i = 0;
+            while (i < clientIds.Count() && dialogsWithClientsIds.Count() != 0)
+            {
+                dialogsWithClients = GetAllDialogsForClient(dialogsWithClientsIds, clientIds.ElementAt(i)).ToList();
+                dialogsWithClientsIds = dialogsWithClients.Select(p => p.IDRGDialog).Distinct().ToList();
+                i++;
+            }
+
+            return dialogsWithClientsIds.Any() ? dialogsWithClientsIds : new List<Guid>() { Guid.Empty };
+        }
+
+        private IEnumerable<RGDialogsClients> GetAllDialogsForClient(IEnumerable<Guid> dialogIds, Guid clientId) =>
+             _clients.Where(p => p.IDClient == clientId && dialogIds.Contains(p.IDRGDialog));
+    }
+}
